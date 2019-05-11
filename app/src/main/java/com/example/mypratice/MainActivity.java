8f3aeb7e5764e64245ea49dc21b0e54af40f00e8
Kavin <kavin851018@gmail.com>
2019-05-11 19:43:46 +0800

    還沒改變成mvvm前

--- app/src/main/java/com/example/mypratice/MainActivity.java
+++ app/src/main/java/com/example/mypratice/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.mypratice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.mypratice.adapters.RecyclerAdapter;
+import com.example.mypratice.models.NicePlace;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapter mAdapter;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFab = findViewById(R.id.fab);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+        mAdapter = new RecyclerAdapter(this, new ArrayList<NicePlace>());
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar(){
+        mProgressBar.setVisibility(View.GONE);
     }
 }
