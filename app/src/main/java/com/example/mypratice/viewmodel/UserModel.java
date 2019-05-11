9cd4ced7e78d1f820ecff4b66c77088846d19a53
Kavin <kavin851018@gmail.com>
2019-05-11 15:11:27 +0800

    完成

--- app/src/main/java/com/example/mypratice/viewmodel/UserModel.java
+++ app/src/main/java/com/example/mypratice/viewmodel/UserModel.java
@@ -1,25 +1,34 @@
 package com.example.mypratice.viewmodel;
 
-public class UserModel {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.mypratice.BR;
+
+public class UserModel extends BaseObservable {
 
     public String email , password ;
 
     public UserModel() {
     }
 
+    @Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
 
+    @Bindable
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+        notifyPropertyChanged(BR.password);
     }
 }
