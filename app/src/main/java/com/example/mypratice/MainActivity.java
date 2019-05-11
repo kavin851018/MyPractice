24f716331b8c91c144ca34b584af03cf99af4981
Kavin <kavin851018@gmail.com>
2019-05-11 14:55:45 +0800

    加入observable之前

--- app/src/main/java/com/example/mypratice/MainActivity.java
+++ app/src/main/java/com/example/mypratice/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.mypratice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.mypratice.databinding.ActivityMainBinding;
+import com.example.mypratice.viewmodel.UserModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this , R.layout.activity_main);
+
+        UserModel userModel = new UserModel();
+        userModel.setEmail("kavin");
+        userModel.setPassword("kavin");
+        activityMainBinding.setUser(userModel);
+
     }
 }
