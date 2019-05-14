49ff0e4b37a48d003a136fc7f795242f01ee97cb
Kavin <kavin851018@gmail.com>
2019-05-14 18:21:14 +0800

    加入fragement recyclerview

--- app/src/main/java/com/example/mypratice/CameraFragement.java
+++ app/src/main/java/com/example/mypratice/CameraFragement.java
@@ -8,12 +8,37 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CameraFragement extends Fragment {
-
+    List<Note> noteList;
+    View v;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragement_camera,container,false);
+
+        v = inflater.inflate(R.layout.fragement_camera,container,false);
+
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
+        NoteAdapter noteAdapter = new NoteAdapter(noteList);
+        recyclerView.setAdapter(noteAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
+
+
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        noteList = new ArrayList<>();
+        noteList.add(new Note ("ok","ok",1));
     }
 }
