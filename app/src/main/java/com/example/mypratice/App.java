2e161fef941b4ec3f35e3677af9ecb7d2d88b74f
Kavin <kavin851018@gmail.com>
2019-05-26 11:40:34 +0800

    完成notification

---
+++ app/src/main/java/com/example/mypratice/App.java
@@ -0,0 +1,47 @@
+package com.example.mypratice;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application {
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannels();
+
+    }
+
+    private void createNotificationChannels(){
+        if(Build.VERSION.SDK_INT >=  Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel.setDescription("This is Channel 1");
+
+            NotificationChannel channe2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Channel 2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel.setDescription("This is Channel 2");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+            manager.createNotificationChannel(channe2);
+
+        }
+
+
+    }
+
+
+}
